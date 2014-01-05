8a0ebe946f4b15f27d83a6f94c875619b4434b3a
Paul Crofts <pmcrofts@margic.com>
2014-01-04 23:41:40 -0700

    trying to debug subprocess...

--- pihex.py
+++ pihex.py
@@ -1,4 +1,6 @@
 import ConfigParser
+import os
+import time
 from sequencer.controller import Controller
 import util
 
@@ -12,7 +14,7 @@ class PiHex():
         self.stdin_path = '/dev/null'
         self.stdout_path = '/dev/tty'
         self.stderr_path = '/dev/tty'
-        self.pidfile_path = '/var/run/pihex/pihex.pid'
+        self.pidfile_path = os.getcwd() + '/pihex.pid'
         self.pidfile_timeout = 5
         # load config properties
         config = ConfigParser.ConfigParser()
@@ -25,8 +27,14 @@ class PiHex():
         self.queue_config['remote_receive_queue'] = config.get('RemoteQueue', 'receive_queue')
         self.queue_config['remote_send_queue'] = config.get('RemoteQueue', 'send_queue')
 
+    def terminate(self):
+        self.log.info('terminated')
+        print 'terminated'
+
     def run(self):
         self.log.info('Starting the JAX robot controller')
+        while True:
+            time.sleep(10)
         # start the controller
         controller = Controller(self.queue_config)
         #controller.start()
