8ad15393f1a23a3f1e878b00465325423c95eda4
Paul Crofts <pmcrofts@margic.com>
2014-01-05 02:08:54 -0700

    modified signal handler to call my terminate method. method to be implemented

--- wrapper.py
+++ wrapper.py
@@ -4,9 +4,9 @@ from pihex import PiHex
 import util
 
 __author__ = 'paul'
-
 pihex = PiHex()
 daemon_runner = DaemonRunner(pihex)
 daemon_runner.daemon_context.working_directory = os.getcwd()
+daemon_runner.daemon_context.signal_map[15] = pihex.terminate
 daemon_runner.daemon_context.files_preserve = [util.file_handler.stream]
 daemon_runner.do_action()
